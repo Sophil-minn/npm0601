7280c8d83d6ebbe876fe380a47b765e42e0c9609
minmin <1056640663@qq.com>
2022-06-05 22:16:01 +0800

    feat: commander 全局配置

--- bin/index.js
+++ bin/index.js
@@ -10,5 +10,16 @@ const pkg = require('../package.json');
 const program = new commander.Command();
 
 program
+  .name(Object.keys(pkg.bin)[0])
+  .usage('<command> [options]')
   .version(pkg.version)
+  .option('-d, --debug', '是否开启调试模式', false)
+  .option('-e, --envName <envName>', '获取环境变量名称')
   .parse(process.argv)
+  program.opts()
+  
+
+  console.log(program.opts().debug);
+  // console.log(program.opts());
+  console.log(program.opts().envName);
+  // console.log(program.outputHelp());
