e3262b0fa1d6dd3b587363716eaca9d19bc7a2f8
minmin <1056640663@qq.com>
2022-06-05 21:37:10 +0800

    feat: yargs 高级用法

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,20 @@
-#!/usr/bin/env node
+
 
 const yargs = require('yargs/yargs');
 
 const { hideBin } = require('yargs/helpers');
 
 const arg = hideBin(process.argv);
+
 const cli = yargs(arg);
+const argv = process.argv.slice(2)
 const dedent = require('dedent');
+const pkg = require('../package.json');
+
+const context = {
+  onceTest: pkg.version,
+  moreArgs: '2022 520'
+}
 
 cli
 .usage('Usage: once-test [command] <options>')
@@ -15,6 +23,11 @@ cli
 .alias("m", "version")
 .alias("e", "debug")
 .strict()
+.fail((err, msg) => {
+  console.log(err, 'err');
+  console.log(msg, 'msg');
+})
+.recommendCommands()
 .wrap(cli.terminalWidth())
 .epilogue(dedent`  1111
 
@@ -39,4 +52,26 @@ cli
 })
 .group(['debug'], 'Dev options')
 .group(['registry'], 'Extra options')
-.argv;
+.command('init [name]', 'Do init a project', (yargs) => {
+  yargs.option(
+    "name", {
+      type: 'string', 
+      describe: 'Name of Project'
+    }
+  )
+
+}, (argv) => {
+  console.log(argv)
+} )
+.command({
+  command: 'List',
+  aliases: ['ls', 'la', 'll'],
+  describe: 'List Local packages',
+  builder: (yargs) => {
+
+  },
+  handler: (argv) => {
+    console.log(argv);
+  }
+ })
+.parse(argv, context)
