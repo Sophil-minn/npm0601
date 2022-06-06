05e965169b87621b9d0949f2a87a3cff55a172ef
minmin <1056640663@qq.com>
2022-06-06 21:59:57 +0800

    feat: command 高级定制

--- bin/index.js
+++ bin/index.js
@@ -47,4 +47,60 @@ service
 
 program.addCommand(service);
 
+program
+  .command('install [name]', 'install package', {
+    // executableFile: 'once-test',
+    hidden: true,
+    // isDefault: true
+  })
+  .alias('i')
+
+program
+  .arguments('<cmd> [options]')
+  .description('test command', {
+    cmd: 'command to run ',
+    options: 'options for command'
+  })
+  .action((cmd, options) => {
+    console.log(cmd, options)
+  })
+
+
+  // 高级定制1： 自定义help信息
+// program.outputHelp(); 方式一 打印帮助信息
+// program.helpInformation() 方式二打印帮助信息
+console.log(program.helpInformation(), 'program.helpInformation()');
+
+// 策略一
+program.helpInformation = function() {
+  return '';
+}
+//  策略二
+program.on('--help', function() {
+  console.log('自定义帮助信息 ');
+});
+// console.log(program.outputHelp(), 'program.outputHelp()');
+
+// 高级定制2 实现debug模式
+program.on('option:debug', function() {
+  console.log('debug', program.opts().debug);
+  if(program.opts().debug) {
+    process.env.LOG_LEVEL = 'verbose'
+  }
+  console.log( process.env.LOG_LEVEL, ' process.env.LOG_LEVEL');
+});
+
+// 高级定制3: 对未知命令监听
+program.on('command:*', function(obj){
+  console.log(obj);
+  console.error('未知的命令：' + obj[0])
+  const avaiableCommands = program.commands.map(cmd => cmd.name());
+  console.log(avaiableCommands);
+  console.log('可用命令：' + avaiableCommands.join(','));
+});
+
+
+
+
+
 program.parse(process.argv)
