2c62c806d8dba636668bf6608e5acb3c52eb38f5
minmin <1056640663@qq.com>
2022-06-12 23:32:49 +0800

    feat: child_process spwan

--- bin/process/index.js
+++ bin/process/index.js
@@ -1,23 +1,23 @@
 const cp = require('child_process');
 const path = require('path');
 
-cp.exec('ls  -al|grep node_modules', function(err, stdout, stderr){
-  console.log(err);
-  console.log(stdout);
-  console.log(stderr);
-});
+// cp.exec('ls  -al|grep node_modules', function(err, stdout, stderr){
+//   console.log(err);
+//   console.log(stdout);
+//   console.log(stderr);
+// });
 
-cp.exec(path.resolve(__dirname, 'test.shell'), {
+// cp.exec(path.resolve(__dirname, 'test.shell'), {
   
-  cwd: path.resolve('../../'),
-  // cwd: process.cwd(),
-}, function(err, stdout, stderr){
-  console.log('cwd')
-  console.log(err);
-  console.log(stdout);
-  console.log(stderr);
-  console.log('cwd')
-});
+//   cwd: path.resolve('../../'),
+//   // cwd: process.cwd(),
+// }, function(err, stdout, stderr){
+//   console.log('cwd')
+//   console.log(err);
+//   console.log(stdout);
+//   console.log(stderr);
+//   console.log('cwd')
+// });
 // 
 // cp.execFile('ls', ['-al'], function(err, stdout, stderr){
 //   console.log(err);
@@ -25,11 +25,28 @@ cp.exec(path.resolve(__dirname, 'test.shell'), {
 //   console.log(stderr);
 // });
 
-cp.execFile(path.resolve(__dirname, 'test.shell'), ['-al',  'cc'], function(err, stdout, stderr){
-  console.log(err);
-  console.log(stdout);
-  console.log(stderr);
-});
+// cp.execFile(path.resolve(__dirname, 'test.shell'), ['-al',  'cc'], function(err, stdout, stderr){
+//   console.log(err);
+//   console.log(stdout);
+//   console.log(stderr);
+// });
 
 
 // console.log(cp, 'cp');
+
+const child = cp.spawn(path.resolve(__dirname, 'test.shell'), ['-al',  'cc'], {
+  cwd: path.resolve('../../'),
+});
+
+child.stdout.on('data', function(chunk){
+  console.log('stdout', chunk.toString());
+});
+child.stderr.on('data', function(chunk){
+  console.log('stderr', chunk.toString());
+});
+
+// spawn: 耗时任务（比如： npm install）, 需要不断打印日志
+
+// exec/execFile：开销比较小的任务
+
+console.log(child.pid, process.pid);
